0887fd29f6265cded00cf9b01992b9b587e6e2df
“yenkelgit config --global user.namgit config --global user.name “yenkel <yenkel.mayost@gmail.com>
2017-03-15 14:11:38 +0200

    fix service

--- public/js/service/movies.js
+++ public/js/service/movies.js
@@ -1,103 +1,5 @@
 app.service('service', function($http) {
 
-    // search with actor's id to find their *credits*
-    var searchPersonId = function(personId) {
-        console.log(personId);
-        var api_key = "8827ea31eb495768c6d732582c199ecc";
-        $http({
-            method: "GET",
-            url: "https://api.themoviedb.org/3/person/" + personId + "/movie_credits?api_key=" + api_key
-        }).then(function successCallback(response) {
-            console.log(response.data.cast);
-            credits = response.data.cast;
-            console.log(people["0"]["0"]);
-
-        }, function errorCallback(data) {
-            console.log(data.data);
-        });
-    };
-
-    //  on actor selection, store their credits to check against next link movie input
-    var credits = [];
-
-    // on movie selection store the movie's cast to check against next link actor input
-    var cast = [];
-
-    //after picking a movie from search, hold the cast information to check against
-    //next link actor input
-    var searchCast = function(movieId) {
-        console.log(movieId);
-        var api_key = "8827ea31eb495768c6d732582c199ecc";
-        $http({
-            method: "GET",
-            url: "https://api.themoviedb.org/3/movie/" + movieId + "/credits?api_key=" + api_key
-        }).then(function successCallback(response) {
-            cast = response.data.cast;
-            console.log(cast["0"]);
-
-        }, function errorCallback(data) {
-            console.log(data.data);
-        });
-
-    };
-
-    //array for showing beginning movie lists
-    var displayRightList = [];
-
-    //function for retrieving popular movies and making display list
-    var searchPopular = function() {
-        console.log("popular is working");
-        var api_key = "8827ea31eb495768c6d732582c199ecc";
-        $http({
-            method: "GET",
-            url: "https://api.themoviedb.org/3/movie/popular?api_key=" + api_key + "&language=en-US&page=1"
-        }).then(function successCallback(response) {
-
-            displayRightList = response.data.results;
-
-
-        }, function errorCallback(data) {
-
-        });
-    };
-
-    //function for retrieving top rated movies and making display list
-    var searchTopRated = function() {
-        console.log("topRated is working");
-        var api_key = "8827ea31eb495768c6d732582c199ecc";
-        $http({
-            method: "GET",
-            url: "https://api.themoviedb.org/3/movie/top_rated?api_key=" + api_key + "&language=en-US&page=1"
-        }).then(function successCallback(response) {
-
-            displayRightList = response.data.results;
-            console.log(topRated["0"]);
-
-        }, function errorCallback(data) {
-
-        });
-    };
-
-    //function for retrieving current movies and making display list
-    var searchNowPlaying = function() {
-        console.log("NowPlaying is working");
-        var api_key = "8827ea31eb495768c6d732582c199ecc";
-        $http({
-            method: "GET",
-            url: "https://api.themoviedb.org/3/movie/now_playing?api_key=" + api_key + "&language=en-US&page=1"
-        }).then(function successCallback(response) {
-
-            displayRightList = response.data.results;
-            console.log(nowPlaying["0"]);
-
-        }, function errorCallback(data) {
-
-        });
-    };
-
-    //array for holding movie search results
-    var resultByTitle = [];
-
     //function for finding list of movie's based on user input search
     var searchTitle = function(titleMovie) {
         console.log(titleMovie);
@@ -115,7 +17,119 @@ app.service('service', function($http) {
         });
     };
 
-    //object and array for shoing user's movie and actor links
+    //array for holding movie search results
+    var resultByTitle = [];
+
+    //after picking a movie from search, hold the cast information to check against
+    //next link actor input
+    var searchCast = function(movieId) {
+        console.log(movieId);
+        var api_key = "8827ea31eb495768c6d732582c199ecc";
+        $http({
+            method: "GET",
+            url: "https://api.themoviedb.org/3/movie/" + movieId + "/credits?api_key=" + api_key
+        }).then(function successCallback(response) {
+            cast = response.data.cast;
+
+        }, function errorCallback(data) {
+            console.log(data.data);
+        });
+
+    };
+
+    // on movie selection store the movie's cast to check against next link actor input
+    var cast = [];
+
+    //function for retrieving results of user input to find actor
+    var searchActor = function(actorName) {
+        var api_key = "8827ea31eb495768c6d732582c199ecc";
+        $http({
+            method: "GET",
+            url: "https://api.themoviedb.org/3/search/person?api_key=" + api_key + "&language=en-US&query=" + actorName + "&page=1&include_adult=false"
+        }).then(function successCallback(response) {
+            resultsByActor = response.data.results;
+
+        }, function errorCallback(data) {
+
+        });
+    };
+
+    //array for listing results of general actor search
+    var resultByActor = [];
+
+    // search with actor's id to find their *credits*
+    var searchPersonId = function(personId) {
+        console.log(personId);
+        var api_key = "8827ea31eb495768c6d732582c199ecc";
+        $http({
+            method: "GET",
+            url: "https://api.themoviedb.org/3/person/" + personId + "/movie_credits?api_key=" + api_key
+        }).then(function successCallback(response) {
+            console.log(response.data.cast);
+            credits = response.data.cast;
+
+        }, function errorCallback(data) {
+            console.log(data.data);
+        });
+    };
+
+    //  on actor selection, store their credits to check against next link movie input
+    var credits = [];
+
+
+    // INITIAL MOVIE LIST DISPLAY
+    //function for retrieving popular movies and making display list
+    var searchPopular = function() {
+        console.log("popular is working");
+        var api_key = "8827ea31eb495768c6d732582c199ecc";
+        $http({
+            method: "GET",
+            url: "https://api.themoviedb.org/3/movie/popular?api_key=" + api_key + "&language=en-US&page=1"
+        }).then(function successCallback(response) {
+            console.log(response.data.results[0]);
+            displayRightList = response.data.results;
+
+        }, function errorCallback(data) {
+
+        });
+    };
+
+    //function for retrieving top rated movies and making display list
+    var searchTopRated = function() {
+        console.log("topRated is working");
+        var api_key = "8827ea31eb495768c6d732582c199ecc";
+        $http({
+            method: "GET",
+            url: "https://api.themoviedb.org/3/movie/top_rated?api_key=" + api_key + "&language=en-US&page=1"
+        }).then(function successCallback(response) {
+            console.log(response.data.results[0]);
+            displayRightList = response.data.results;
+        }, function errorCallback(data) {
+
+        });
+    };
+
+    //function for retrieving current movies and making display list
+    var searchNowPlaying = function() {
+        var api_key = "8827ea31eb495768c6d732582c199ecc";
+        $http({
+            method: "GET",
+            url: "https://api.themoviedb.org/3/movie/now_playing?api_key=" + api_key + "&language=en-US&page=1"
+        }).then(function successCallback(response) {
+            console.log(response.data.results[0]);
+            displayRightList = response.data.results;
+
+        }, function errorCallback(data) {
+
+        });
+    };
+
+    //array for showing beginning movie lists
+    var displayRightList = [];
+
+
+    //LINKS
+    //object and array for showing user's movie and actor links/selections
     var links = {
         movies: [{
             title: "The Grinch",
@@ -145,22 +159,7 @@ app.service('service', function($http) {
         links.actors.push(actor);
     };
 
-    //array for listing results of general actor search
-    var resultByActor = [];
-    var searchActor = function(actorName) {
-        var api_key = "8827ea31eb495768c6d732582c199ecc";
-        $http({
-            method: "GET",
-            url: "https://api.themoviedb.org/3/search/person?api_key=" + api_key + "&language=en-US&query=" + actorName + "&page=1&include_adult=false"
-        }).then(function successCallback(response) {
 
-            resultsByActor = response.data.results;
-            console.log(actors["0"]);
-
-        }, function errorCallback(data) {
-
-        });
-    };
 
     //exporting functions to the controller
     return {
