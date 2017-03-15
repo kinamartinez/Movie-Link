9180ffdc7b025c6d1df45480695f8516d297a19a
“yenkelgit config --global user.namgit config --global user.name “yenkel <yenkel.mayost@gmail.com>
2017-03-15 16:54:03 +0200

    play variable

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -20,12 +20,19 @@ app.controller('mainController', function($scope, service) {
         service.searchTitle($scope.titleMovie);
     };
 
-    $scope.test = function () {
+    $scope.test = function() {
         console.log(this.movie.id);
     };
 
     //Invoke get Popular movies to display when the page opens
     $scope.searchPopular();
+
+    //Variables to show/hide the pages
+    $scope.play = false;
+
+    $scope.playing = function() {
+        $scope.play = true;
+    };
 
 
 });
