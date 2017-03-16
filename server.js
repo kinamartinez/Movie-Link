192916c768dca5a45c325b2d361bdc550c646429
“yenkelgit config --global user.namgit config --global user.name “yenkel <yenkel.mayost@gmail.com>
2017-03-16 10:21:08 +0200

    api routes

--- server.js
+++ server.js
@@ -21,6 +21,58 @@ var port = /*process.env.PORT ||*/ 8080;
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 
+//API routes
+app.get('/collection', function(req, res, next) {
+    Movie.find(function(error, movies) {
+        if (error) {
+            console.error(error)
+            return next(error);
+        } else {
+            res.send(movies);
+        }
+    });
+});
+
+app.post('/collection', function(req, res, next) {
+    var movie = new Movie(req.body);
+
+    movie.save(function(err, movie) {
+        if (err) {
+            console.error(err)
+            return next(err);
+        } else {
+            res.json(movie);
+        }
+    });
+});
+
+app.delete('/collection/:id', function(req, res, next) {
+    //Movie.find({_id: req.par})
+    Movie.remove({ _id: req.params.id }, function(err) {
+        if (err) {
+            console.error(err)
+            return next(err);
+        } else {
+            res.json("Movie Deleted!");
+        }
+    });
+});
+
+app.put('/collection/:id', function(req, res, next) {
+    Movie.findOneAndUpdate({ _id: req.param.id }, req.body, { new: true }, function(err, movie) {
+        if (err) {
+            console.error(err)
+            return next(err);
+        } else {
+            res.send(movie);
+        }
+    });
+});
+
+
+
+
+
 // error handler to catch 404 and forward to main error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
